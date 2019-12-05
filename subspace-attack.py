ad4fa7429cb367be7e16ce54b9ce22b9b9238c8c
Edoardo Debenedetti <edoardo.m.debenedetti@gmail.com>
2019-12-05 14:19:01 +0100

    Add first gradient difference check

--- subspace-attack.py
+++ subspace-attack.py
@@ -6,14 +6,18 @@ import numpy as np
 from src.import_models import load_model, MODELS_DATA, MODELS_DIRECTORY
 from src.plots import imshow
 
+
 def main():
-    # Load MNIST dataset
+
+    # Load CIFAR10 dataset
     preprocess = transforms.Compose([
         transforms.ToTensor(),
     ])
 
-    data = datasets.CIFAR10(root='./data', train=True, download=True, transform=preprocess)
-    data_loader = torch.utils.data.DataLoader(data, batch_size=1, shuffle=True, num_workers=2)
+    data = datasets.CIFAR10(root='./data', train=True,
+                            download=True, transform=preprocess)
+    data_loader = torch.utils.data.DataLoader(
+        data, batch_size=1, shuffle=True, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
@@ -21,20 +25,22 @@ def main():
     num_classes = len(classes)
 
     # Load reference models
-    reference_model_names = ['vgg11_bn', 'vgg13_bn', 'vgg16_bn', 'vgg16_bn', 'vgg19_bn', 'AlexNet_bn']
-    reference_models = list(map(lambda name: load_model(MODELS_DIRECTORY, MODELS_DATA, name, num_classes), reference_model_names))
-
-
+    reference_model_names = ['vgg11_bn', 'vgg13_bn',
+                             'vgg16_bn', 'vgg19_bn', 'AlexNet_bn']
+    reference_models = list(map(lambda name: load_model(
+        MODELS_DIRECTORY, MODELS_DATA, name, num_classes), reference_model_names))
 
     # Load victim model
     victim_model_name = 'gdas'
-    victim_model = load_model(MODELS_DIRECTORY, MODELS_DATA, victim_model_name, num_classes)
+    victim_model = load_model(
+        MODELS_DIRECTORY, MODELS_DATA, victim_model_name, num_classes)
     _ = victim_model.eval()
 
     if torch.cuda.is_available():
-        reference_models = list(map(lambda model: model.to('cuda'), reference_models))
+        reference_models = list(
+            map(lambda model: model.to('cuda'), reference_models))
         victim_model = victim_model.to('cuda')
-        
+
     # Hyper-parameters
     tau = 1
     epsilon = 8 / 255
@@ -51,7 +57,8 @@ def main():
         print(f'\n-------------\n')
         print(f'Target image number {counter}')
 
-        queries_counter = attack(data, target, tau, epsilon, delta, eta_g, eta, victim_model, reference_models, verbose=True)
+        queries_counter = attack(data, target, tau, epsilon, delta,
+                                 eta_g, eta, victim_model, reference_models, verbose=True)
 
         counter += 1
 
@@ -59,7 +66,7 @@ def main():
 
         if counter == limit:
             break
-            
+
     results = np.array(queries)
     failed = results == -1
 
@@ -69,24 +76,35 @@ def main():
     print(f'Median number of queries: {np.median(results[~failed])}')
     print(f'Number of failed queries: {len(results[failed])}')
     print(f'\n-------------\n')
-    
-    
-def attack(input_batch, true_label, tau, epsilon, delta, eta_g, eta, victim, references, limit=10000, verbose=False, show_images=False):
-    
+
+
+def attack(input_batch, true_label, tau, epsilon, delta, eta_g, eta, victim, references, limit=10000, verbose=False, show_images=False, truest_label=True):
+
     # Regulators
     regmin = input_batch - epsilon
     regmax = input_batch + epsilon
 
-    # Initialize the adversarial example 
+    # Initialize the adversarial example
     x_adv = input_batch.clone()
-    y = true_label
+    x_adv.requires_grad_(True)
+    victim.zero_grad()
+    
+    victim.eval()
+    predicted_y = victim(x_adv)
 
-    # setting a example label
+    # Set the label to be used (the predicted one vs the true one)
+    y = true_label if truest_label else predicted_y
 
+    # Set CrossEntropy loss
     criterion = torch.nn.CrossEntropyLoss()
 
-    #Initialize the gradient to be estimated
-    g = torch.zeros_like(input_batch) # check the dimensions
+    # Compute the true first gradient, for comparison purposes
+    true_first_loss = criterion(predicted_y, y)
+    true_first_loss.backward()
+    true_first_gradient = x_adv.grad
+
+    # Initialize the gradient to be estimated
+    g = torch.zeros_like(input_batch)
 
     # move the input and model to GPU for speed if available
     if torch.cuda.is_available():
@@ -98,7 +116,7 @@ def attack(input_batch, true_label, tau, epsilon, delta, eta_g, eta, victim, ref
 
     # initialize quiries counter
     q_counter = 0
-    
+
     if show_images:
         with torch.no_grad():
             imshow(x_adv[0].cpu())
@@ -106,28 +124,32 @@ def attack(input_batch, true_label, tau, epsilon, delta, eta_g, eta, victim, ref
     success = False
 
     while not success and q_counter < limit:
-        
+
         if q_counter % 50 == 0 and verbose:
             # imshow(x_adv[0].cpu())
             print(f'Iteration number: {str(q_counter / 2)}')
             print(f'{str(q_counter)} queries have been made')
-        
+
         # Load random reference model
         random_model = random.randint(0, len(references) - 1)
-        model = references[random_model]
-        model.eval()
+        reference_model = references[random_model]
+        reference_model.eval()
 
         # calculate the prior gradient - L8
         x_adv.requires_grad_(True)
-
-        model.zero_grad()
-        output = model(x_adv)
+        reference_model.zero_grad()
+        output = reference_model(x_adv)
 
         loss = criterion(output, y)
 
         loss.backward()
 
         u = x_adv.grad
+
+        if q_counter == 0:
+            with torch.no_grad():
+                init_grad_diff = u - true_first_gradient
+                print(f'Initial gradient difference (l-2 norm): {init_grad_diff.data.norm()}')
 
         # Calculate delta - L11
         with torch.no_grad():
@@ -142,26 +164,27 @@ def attack(input_batch, true_label, tau, epsilon, delta, eta_g, eta, victim, ref
 
             query_minus = victim(x_minus)
             query_plus = victim(x_plus)
-        
-        q_counter += 2        
 
-        delta_t = ((criterion(query_plus, y) - criterion(query_minus, y)) / (tau * epsilon)) * u
+        q_counter += 2
+
+        delta_t = ((criterion(query_plus, y) -
+                    criterion(query_minus, y)) / (tau * epsilon)) * u
 
         # Update gradient - L12
         g = g + eta_g * delta_t
-        
+
         # Update the adverserial example - L13-15
         with torch.no_grad():
             x_adv = x_adv + eta * torch.sign(g)
             x_adv = torch.max(x_adv, regmin)
             x_adv = torch.min(x_adv, regmax)
             x_adv = torch.clamp(x_adv, 0, 1)
-            
+
             # Check success
             label_minus = query_minus.max(1, keepdim=True)[1].item()
             label_plus = query_plus.max(1, keepdim=True)[1].item()
 
-        if  label_minus != true_label.item() or label_plus != true_label.item():
+        if label_minus != true_label.item() or label_plus != true_label.item():
             print('Success! after {} queries'.format(q_counter))
             print("True: {}".format(true_label.item()))
             print("Label minus: {}".format(label_minus))
@@ -171,6 +194,7 @@ def attack(input_batch, true_label, tau, epsilon, delta, eta_g, eta, victim, ref
             success = True
 
     return q_counter if success else -1
+
 
 if __name__ == '__main__':
     main()
